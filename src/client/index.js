94f5f3b4c867fc55f211fa12f6ad5a46a595e9e8
sv-law <svasti.law0@gmail.com>
2023-10-21 15:31:05 +1100

    Progress, I guess. Consider reverting, index.html when using npm run start can't find the main.js file (404)

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,8 @@
-alert("I EXIST")
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+console.log(checkForName);
+
+console.log('hello');
+
+alert("I EXIST");
