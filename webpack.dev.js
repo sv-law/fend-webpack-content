ce8b2022cef5488ea5b6b5ea9447f991e1b62a75
sv-law <svasti.law0@gmail.com>
2023-10-24 00:57:33 +1100

    Added comment about HtmlWebpackPlugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -18,6 +18,9 @@ module.exports = {
         ]
     },
     plugins: [
+        // CleanWebpackPlugin rebuilds the `dist` folder contents
+        // without having the user delete the folder manually or 
+        // increasing the length of the command used via CLI.
         new CleanWebpackPlugin({
             // Simulate the removal of files
             dry: true,
@@ -27,6 +30,11 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         }),
+        // HtmlWebpackPlugin allows one to /generate/ an HTML file, 
+        // that has JS and so forth in it added dynamically; no need 
+        // to manually add all the separate JS files.
+        // Result in `dist/index.html`
+        //<script type="text/javascript" src="main.js"></script></body>
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
